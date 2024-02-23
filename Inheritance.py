6394cca0f012ca8ca92843f4c70665f0f89d4257
Nilay Güler <gulernilay088@gmail.com>
2024-02-23 22:26:52 +0300

    Inheritance/OOP is added

---
+++ Inheritance.py
@@ -0,0 +1,40 @@
+"""
+
+
+"""
+class Person :
+  village="Izmir" #static =constant in every object
+
+  def __init__(self,name,lastname,age):
+    self.name=name
+    self.lastname=lastname
+    self.age=age
+  
+  def eat():
+    pass
+
+  def run():
+    pass
+
+
+class Student(Person):
+  def __init__(self):
+    pass
+class Teacher(Person):
+  def __init__(self):
+    pass
+  
+s1=Student("Kadir","Halil",34)
+s1.eat()
+s1.run()
+t1=Teacher("Uygar","Salman",45)
+t1.eat()
+t1.run()
+
+
+
+
+
+
+
+ 
